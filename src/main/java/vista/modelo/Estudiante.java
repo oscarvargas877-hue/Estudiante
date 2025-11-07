0cb00c04d2c10be77b540840d20a7ee2cff0d2f3
oscar100dav <oscar.vargas877@ist17dejulio.edu.ec>
2025-11-07 13:39:41 -0500

    ejercicio con interfaz

--- src/main/java/vista/modelo/Estudiante.java
+++ src/main/java/vista/modelo/Estudiante.java
@@ -9,5 +9,46 @@ package vista.modelo;
  * @author Usuario
  */
 public class Estudiante {
+    private String nombre;
+    private String edad;
+    private String correo;
+
+    public Estudiante(String nombre, String edad ,String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo=correo;
+    }
+    
+    //metodos
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    public void imprimir(){
+        System.out.println("nombre: "+ nombre + " edad: "+edad + " Correo: " + correo);
+    
+    }
+    
     
 }
---
+++ src/main/java/vista/modelo/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista.modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+    public static void main(String[] args) {
+        Estudiante e= new Estudiante("Oscar", "23", "oscar@gmail.com");
+        e.imprimir();
+    }
+    
+}
